f6b0fc32e4fcbd774d9995a33c38b7a7ca918ede
Ashley Archibald <aarchibald20@gmail.com>
2019-02-15 01:15:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,15 +8,15 @@ def summon_captain_planet(produce)# code an argument here
   produce.collect {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls(calls)# code an argument here
+def long_planeteer_calls(call)# code an argument here
   # Your code here
-   calls.any? {|word| word.length > 4}
+   call.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arrays)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |word|
+  arrays.find do |word|
    cheese_types.find {|cheese| cheese == word}
    end
 end
